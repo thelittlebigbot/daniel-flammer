3cf87e582a2aaacb02e24cfd895870072cb4ed43
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 00:46:21 +0100

    feat: create default layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { routes, style } from '../../helpers';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const Layout: React.FC<Props> = ({ children }) => {
+  const renderNavigationLinks = routes
+    .sort((a, b) => a.index - b.index)
+    .map((i, key) => {
+      if (i.visible) {
+        return (
+          <NavLink key={key} exact to={i.path} activeClassName='active'>
+            {i.name}
+          </NavLink>
+        );
+      } else return null;
+    });
+
+  return (
+    <Component>
+      <Container>
+        <Header>
+          <Logo>[LOGO]</Logo>
+          <Navigation>{renderNavigationLinks}</Navigation>
+        </Header>
+        <Main>{children}</Main>
+      </Container>
+    </Component>
+  );
+};
+
+const Component = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  display: flex;
+  height: ${() => style.font.size.big};
+  font-size: ${() => style.font.size.medium};
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.p`
+  color: red;
+  font-weight: bold;
+`;
+
+const Navigation = styled.nav`
+  & a {
+    color: ${() => style.color.black};
+    margin-right: ${() => style.font.size.regular};
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+`;
+
+const Main = styled.main`
+  margin: ${() => style.font.size.big} 0;
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Layout } from './components';
 import { Routes, Style } from './helpers';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Layout>
       <Routes />
-    </div>
+    </Layout>
   );
 };
 
