8a5c8d5ae96e17490e48c0b5d860540a7872cc0e
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 03:00:10 +0100

    feat: render resources data in artboard route without style

--- src/routes/artboard/index.tsx
+++ src/routes/artboard/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import { resourcesData } from '../../data';
+
 export const Artboard: React.FC = () => {
+  const renderResources = resourcesData
+    .sort((a, b) => b.date - a.date)
+    .map((i, key) => {
+      return (
+        <div>
+          <p>{i.date}</p>
+          <div>
+            {i.resources.map((i, key) => {
+              return <img src={i.path} alt={i.tag} />;
+            })}
+          </div>
+        </div>
+      );
+    });
+
   return (
     <div>
       <h1>Artboard</h1>
+      {renderResources}
     </div>
   );
 };
