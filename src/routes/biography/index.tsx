550f8bbf9e372c3f07ed2d24138f63d3a27ee19c
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 01:12:41 +0100

    feat: render biography data without style

--- src/routes/biography/index.tsx
+++ src/routes/biography/index.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
 
+import { biographyData } from '../../data';
+
 export const Biography: React.FC = () => {
-  return <div></div>;
+  const renderEducatonSection = biographyData.education.map((i, key) => {
+    return (
+      <div key={key}>
+        <h3>{i.school}</h3>
+        <h4>{i.degree}</h4>
+        <p>{i.date}</p>
+      </div>
+    );
+  });
+
+  const renderExhibitionSection = biographyData.exhibition.map((i, key) => {
+    return (
+      <div key={key}>
+        <h3>{i.name}</h3>
+        <h4>{i.place}</h4>
+        <p>{i.date}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <h2>About me</h2>
+        <p>{biographyData.about.first}</p>
+        <p>{biographyData.about.second}</p>
+      </div>
+      <div>
+        <h2>Education</h2>
+        {renderEducatonSection}
+      </div>
+      <div>
+        <h2>Exhibition</h2>
+        {renderExhibitionSection}
+      </div>
+    </div>
+  );
 };
