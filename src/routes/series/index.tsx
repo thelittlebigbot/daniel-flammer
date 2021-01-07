8bde66b0718a639a7939e3184db8c97ca2c12346
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 03:07:58 +0100

    feat: render resources data to series without style

--- src/routes/series/index.tsx
+++ src/routes/series/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { resourcesData } from '../../data';
 
 export const Series: React.FC = () => {
+  const renderResources = resourcesData.map((i, key) => {
+    return (
+      <Link to={i.link}>
+        <div>
+          <img src={i.thumb} alt={i.name} />
+          <h2>{i.name}</h2>
+          <p>{i.date}</p>
+        </div>
+      </Link>
+    );
+  });
+
   return (
     <div>
       <h1>Series</h1>
+      {renderResources}
     </div>
   );
 };
