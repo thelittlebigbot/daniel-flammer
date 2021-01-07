a1452b0be3a22b50f2b3fa6eb8631852a3c9cbad
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 05:05:38 +0100

    feat: add a tag

--- src/routes/map/index.tsx
+++ src/routes/map/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Page, Typography } from '../../components';
 
 export const Map: React.FC = () => {
-  return <div></div>;
+  return (
+    <Page>
+      <Typography type='title' variant='big'>
+        Carte mentale en cours de développement
+      </Typography>
+    </Page>
+  );
 };
