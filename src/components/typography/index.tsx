4ef19cc717f3ff37d23f8b4b25b197e6fb287fdf
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 04:04:59 +0100

    feat: add font weight on typography component

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -35,33 +35,41 @@ export const Typography: React.FC<Props> = ({ children, type, variant }) => {
 
 const Text = styled.p`
   font-size: ${() => style.font.size.regular};
+  font-weight: ${() => style.font.weight.regular};
 `;
 
 const TextSmall = styled.p`
   font-size: ${() => style.font.size.small};
+  font-weight: ${() => style.font.weight.light};
 `;
 
 const TitleSmall = styled.h5`
   font-size: ${() => style.font.size.small};
   margin-bottom: ${() => style.font.size.small};
+  font-weight: ${() => style.font.weight.medium};
 `;
 
 const TitleRegular = styled.h4`
   font-size: ${() => style.font.size.regular};
   margin-bottom: ${() => style.font.size.regular};
+  font-weight: ${() => style.font.weight.semiBold};
 `;
 
 const TitleMedium = styled.h3`
   font-size: ${() => style.font.size.medium};
   margin-bottom: ${() => style.font.size.medium};
+  font-weight: ${() => style.font.weight.bold};
 `;
 
 const TitleLarge = styled.h2`
   font-size: ${() => style.font.size.large};
   margin-bottom: ${() => style.font.size.large};
+  font-weight: ${() => style.font.weight.extraBold};
 `;
 
 const TitleBig = styled.h1`
   font-size: ${() => style.font.size.big};
   margin-bottom: ${() => style.font.size.big};
+  font-weight: ${() => style.font.weight.black};
+  text-shadow: ${() => style.shadow};
 `;
