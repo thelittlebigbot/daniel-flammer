93bdb5c1deff8434784acace82bda8c124e43761
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 01:05:21 +0100

    feat: create a data file for biography content

---
+++ src/data/biography.ts
@@ -0,0 +1,42 @@
+export const biographyData = {
+  about: {
+    first:
+      'Daniel Flammer continues in his artistic expression the great tradition of work on canvas and paper, with recent incursions in the direction of three-dimensional installation and video. His artistic approach is more about mental cosa than pure representation, in a practice nourished by references that takes collage, metaphor, mind games. He integrates motifs deprived of their initial meaning in a pictorial language of their own: dreamlike worlds saturated with colors, impregnated with surrealist technology, populated by a mythological bestiary and segmented bodies. His production is halfway between personal catharsis and reflection on contemporary society . The series Entre les narrations et Déshumanités is a search for motifs, an exploration of the space of painting. They are crossed by a strong relationship to words, to writing, in narrations in the form of enigmas or rebus. A second set confronts the heritage of master paintings to the everyday world, in a painting that brings together Bosch, Brueghel or the New School of Leipzig, but also the merchants of a neighborhood, street furniture, real and fictitious landscapes.',
+    second:
+      'Born in 1984, Daniel Flammer graduated in 2009 from ENSBA with the congratulations of the jury. Represented since 2013 by the Polad-Hardouin gallery, he exhibits in France and abroad. His personal story feeds his work: intellectual heritage of a mother painter and a father psychiatrist, memories of a childhood spent in workshops, family dramas. Daniel Flammer also enriches his work with other approaches: scenography, decor, literature, music. His journey is also an itinerary of travels, family, then artistic and personal, from Paris to Berlin, from Argentina to Iceland, mental reservoirs of stories and landscapes. Daniel Flammer lives and works in Paris. Around recurring reflections and motifs, three movements emerge in his work. The first is that of a reflexive return on art, its mediums and tools. The painting which proceeds from it is that of an objectified everyday world, emptied of its meaning and populated with deconstructed bodies, borrowing from Giorgio De Chirico and Dada collages.',
+  },
+  education: [
+    {
+      date: '01/01/2000',
+      degree: 'Degree name',
+      school: 'School name',
+    },
+    {
+      date: '01/01/2000',
+      degree: 'Degree name',
+      school: 'School name',
+    },
+    {
+      date: '01/01/2000',
+      degree: 'Degree name',
+      school: 'School name',
+    },
+  ],
+  exhibition: [
+    {
+      date: '01/01/2000',
+      name: 'Exhibition name',
+      place: 'Paris, France',
+    },
+    {
+      date: '01/01/2000',
+      name: 'Exhibition name',
+      place: 'Paris, France',
+    },
+    {
+      date: '01/01/2000',
+      name: 'Exhibition name',
+      place: 'Paris, France',
+    },
+  ],
+};
---
+++ src/data/index.ts
@@ -0,0 +1,3 @@
+import { biographyData } from './biography';
+
+export { biographyData };
