47c4eb3303aca3f2e1eac84ea2f6792777259c93
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-07 03:41:26 +0100

    fix: remove unused things

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { Row } from '../../components';
 import { routes, style } from '../../helpers';
 
 interface Props {
