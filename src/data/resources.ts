921d94169e19c7a0816a618b151f6f799a222bb8
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-11 19:24:22 +0100

    fix: error in description

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -120,7 +120,7 @@ export const resourcesData = [
   },
   {
     date: 2010,
-    decription:
+    description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     link: '/series',
     name: 'Entre les peintres',
