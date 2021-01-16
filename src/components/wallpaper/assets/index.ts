3dd552b575a969b1d0df8c449cc79fcfa114b1b7
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-16 15:40:27 +0100

    feat: add different wallpaper for each route

--- src/components/wallpaper/assets/index.ts
+++ src/components/wallpaper/assets/index.ts
@@ -1,1 +1,15 @@
-export {};
+import biographyWallpaper from './biography.svg';
+import contactWallpaper from './contact.svg';
+import newsWallpaper from './news.svg';
+import publicationsWallpaper from './publication.svg';
+import seriesWallpaper from './series.svg';
+import slugWallpaper from './slug.svg';
+
+export {
+  biographyWallpaper,
+  contactWallpaper,
+  newsWallpaper,
+  publicationsWallpaper,
+  seriesWallpaper,
+  slugWallpaper,
+};
--- src/components/wallpaper/index.tsx
+++ src/components/wallpaper/index.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Wallpaper: React.FC = () => {
-  return null;
+import {
+  biographyWallpaper,
+  contactWallpaper,
+  newsWallpaper,
+  publicationsWallpaper,
+  seriesWallpaper,
+  slugWallpaper,
+} from './assets';
+
+interface Props {
+  route: 'biography' | 'contact' | 'news' | 'publications' | 'series' | 'slug';
+}
+
+export const Wallpaper: React.FC<Props> = ({ route }) => {
+  if (route === 'biography') {
+    return <Component src={biographyWallpaper} alt='Biography wallpaper' />;
+  } else if (route === 'contact') {
+    return <Component src={contactWallpaper} alt='Contact wallpaper' />;
+  } else if (route === 'news') {
+    return <Component src={newsWallpaper} alt='News wallpaper' />;
+  } else if (route === 'publications') {
+    return (
+      <Component src={publicationsWallpaper} alt='Publications wallpaper' />
+    );
+  } else if (route === 'series') {
+    return <Component src={seriesWallpaper} alt='Series wallpaper' />;
+  } else if (route === 'slug') {
+    return <Component src={slugWallpaper} alt='Slug wallpaper' />;
+  } else return null;
 };
+
+const Component = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
