4f66791f12f02a912196e158cdadd99c40de7024
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-16 18:16:10 +0100

    refactor: export assets

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,10 +1,19 @@
 import logo from './logo/logo.png';
 
+// import mapHover from './map/map-hover.svg';
+
+import mapBackground from './map/map-background.svg';
+import mapBiography from './map/map-biography.svg';
+import mapContact from './map/map-contact.svg';
+import mapNews from './map/map-news.svg';
+import mapPublications from './map/map-publications.svg';
+import mapSeries from './map/map-series.svg';
+import mapTitles from './map/map-titles.svg';
+
 import biographyShape from './shapes/biography.svg';
 import contactShape from './shapes/contact.svg';
 import climbShape from './shapes/climb.svg';
 import loopShape from './shapes/loop.svg';
-import mindmapShape from './shapes/mindmap.svg';
 import newsShape from './shapes/news.svg';
 import publicationsShape from './shapes/publications.svg';
 import seriesShape from './shapes/series.svg';
@@ -16,7 +25,13 @@ export {
   climbShape,
   logo,
   loopShape,
-  mindmapShape,
+  mapBackground,
+  mapBiography,
+  mapContact,
+  mapNews,
+  mapPublications,
+  mapSeries,
+  mapTitles,
   newsShape,
   publicationsShape,
   seriesShape,
