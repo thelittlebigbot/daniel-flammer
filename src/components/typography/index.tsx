a879a2deb2bc89b3eed00be8611654588245d8b3
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-23 18:05:51 +0100

    fix: add font-family to all items

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -78,40 +78,47 @@ export const Typography: React.FC<Props> = ({ children, type, variant }) => {
 };
 
 const Text = styled.p`
+  font-family: ${() => style.font.regular};
   font-size: ${() => style.font.size.regular};
   font-weight: ${() => style.font.weight.regular};
 `;
 
 const TextSmall = styled.p`
+  font-family: ${() => style.font.light};
   font-size: ${() => style.font.size.small};
   font-weight: ${() => style.font.weight.light};
 `;
 
 const TitleSmall = styled.h5`
+  font-family: ${() => style.font.medium};
   font-size: ${() => style.font.size.small};
   margin-bottom: ${() => style.font.size.small};
   font-weight: ${() => style.font.weight.medium};
 `;
 
 const TitleRegular = styled.h4`
+  font-family: ${() => style.font.bold};
   font-size: ${() => style.font.size.regular};
   margin-bottom: ${() => style.font.size.regular};
   font-weight: ${() => style.font.weight.semiBold};
 `;
 
 const TitleMedium = styled.h3`
+  font-family: ${() => style.font.bold};
   font-size: ${() => style.font.size.medium};
   margin-bottom: ${() => style.font.size.medium};
   font-weight: ${() => style.font.weight.bold};
 `;
 
 const TitleLarge = styled.h2`
+  font-family: ${() => style.font.heavy};
   font-size: ${() => style.font.size.large};
   margin-bottom: ${() => style.font.size.large};
   font-weight: ${() => style.font.weight.extraBold};
 `;
 
 const TitleBig = styled.h1`
+  font-family: ${() => style.font.extraBold};
   font-size: ${() => style.font.size.big};
   margin-bottom: ${() => style.font.size.medium};
   font-weight: ${() => style.font.weight.black};
