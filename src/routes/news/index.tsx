88ff331c8a0b4890db530fd9807d8cdd07484def
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-24 16:13:24 +0100

    feat: add content to news

--- src/routes/news/index.tsx
+++ src/routes/news/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { newsShape } from '../../assets';
+import styled from 'styled-components';
 
-import { Arrow, Page, SEO } from '../../components';
+import { newsShape } from '../../assets';
+import { Arrow, Page, SEO, Typography } from '../../components';
 
 export const News: React.FC = () => {
   return (
     <Page shape={newsShape}>
       <SEO title='News' description='...' keywords='...' />
+      <Typography type='title' variant='large'>
+        Printemps 2021: Visite de l'ADIAF à l'atelier
+      </Typography>
+      <Image src='/resources/2019-guerre-des-images/6.jpg' alt='News' />
       <Arrow />
     </Page>
   );
 };
+
+const Image = styled.img`
+  width: 50%;
+`;
