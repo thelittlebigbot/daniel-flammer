9b09d2551905ee11442f25abced82d67d2357bce
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-31 15:31:22 +0100

    feat: export publications data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import { biographyData } from './biography';
 import { contactData } from './contact';
+import { publicationsData } from './publications';
 import { resourcesData } from './resources';
 
-export { biographyData, contactData, resourcesData };
+export { biographyData, contactData, publicationsData, resourcesData };
