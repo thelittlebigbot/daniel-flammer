0614c3b991f6c756945fc53e2a6b9cd3b304754d
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-31 15:32:01 +0100

    feat: create publication content

--- src/routes/publications/index.tsx
+++ src/routes/publications/index.tsx
@@ -1,13 +1,68 @@
 import React from 'react';
-import { publicationsShape } from '../../assets';
+import styled from 'styled-components';
 
-import { Arrow, Page, SEO } from '../../components';
+import { publicationsShape } from '../../assets';
+import { Arrow, Page, Typography, SEO, Row } from '../../components';
+import { publicationsData } from '../../data';
 
 export const Publications: React.FC = () => {
+  const renderPublications = publicationsData
+    .sort((a: any, b: any) => b.date - a.date)
+    .map((i, key) => {
+      const PDF = () => {
+        if (i.pdf) {
+          return (
+            <form method='get' action={i.pdf}>
+              <Button type='submit'>Download PDF</Button>
+            </form>
+          );
+        } else return null;
+      };
+
+      return (
+        <Component key={key}>
+          <Row>
+            <Image src={i.thumb} alt={i.name} />
+            <Space />
+            <Typography type='text' variant='italic'>
+              {i.date}
+            </Typography>
+            <Typography type='text' variant='bold'>
+              {i.name}
+            </Typography>
+            <Typography type='text' variant='italic'>
+              {i.subtitle}
+            </Typography>
+            <Typography type='text' variant='regular'>
+              {i.editor}
+            </Typography>
+            <PDF />
+          </Row>
+        </Component>
+      );
+    });
+
   return (
     <Page shape={publicationsShape}>
       <SEO title='Publications' description='...' keywords='...' />
+      {renderPublications}
       <Arrow />
     </Page>
   );
 };
+
+const Component = styled.div`
+  width: 50%;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const Space = styled.div`
+  height: 2rem;
+`;
+
+const Button = styled.button`
+  margin-top: 1rem;
+`;
