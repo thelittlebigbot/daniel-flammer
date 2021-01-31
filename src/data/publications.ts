a29cce05fe4c70341a682744e4bbfcf392306bb2
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-31 15:04:56 +0100

    refactor: manage resources folder

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -4,7 +4,7 @@ export const publicationsData = [
     editor: 'Ecole nationale supérieure des Beaux-arts',
     subtitle: 'Catalogue des Félicités 2009 de Marc Desgrandchamps',
     date: 2010,
-    thumb: '',
+    thumb: '/resources/publications/2010-mouvements-des-atomes/thumb.jpg',
     pdf: '',
   },
   {
@@ -12,7 +12,7 @@ export const publicationsData = [
     subtitle: "Catalogue d'exposition",
     editor: '',
     date: 2013,
-    thumb: '',
+    thumb: '/resources/publications/2013-drawing-now/thumb.jpg',
     pdf: '',
   },
   {
@@ -20,15 +20,15 @@ export const publicationsData = [
     subtitle: "Catalogue d'exposition",
     editor: 'La lyonnaise des Beaux-arts',
     date: 2016,
-    thumb: '',
+    thumb: '/resources/publicatoins/2016-art-paper/thumb.jpg',
     pdf: '',
   },
   {
-    name: "Les anneaux musicaux, catalogue d'exposition ",
+    name: 'Les anneaux musicaux',
     subtitle: "Catalogue d'exposition",
     editor: 'Galerie Polad-Hadouin',
     date: 2016,
-    thumb: '',
-    pdf: '',
+    thumb: '/resources/publications/2016-les-anneaux-musicaux/thumb.jpg',
+    pdf: '/resources/publications/2016-les-anneaux-musicaux/file.jpg',
   },
 ];
