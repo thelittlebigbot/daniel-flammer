5f4ba7780d918efb7d55204a635f4157a4a0b8a8
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-31 14:26:02 +0100

    feat: add more typography variants

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -7,13 +7,24 @@ import { animationDuration, style } from '../../helpers';
 interface Props {
   children: string | number;
   type?: 'text' | 'title';
-  variant?: 'small' | 'regular' | 'medium' | 'large' | 'big';
+  variant?:
+    | 'small'
+    | 'regular'
+    | 'medium'
+    | 'large'
+    | 'big'
+    | 'italic'
+    | 'bold';
 }
 
 export const Typography: React.FC<Props> = ({ children, type, variant }) => {
   if (type === 'text') {
     if (variant === 'small') {
       return <TextSmall>{children}</TextSmall>;
+    } else if (variant === 'bold') {
+      return <TextBold>{children}</TextBold>;
+    } else if (variant === 'italic') {
+      return <TextItalic>{children}</TextItalic>;
     } else return <Text>{children}</Text>;
   }
 
@@ -80,47 +91,50 @@ export const Typography: React.FC<Props> = ({ children, type, variant }) => {
 const Text = styled.p`
   font-family: ${() => style.font.regular};
   font-size: ${() => style.font.size.regular};
-  font-weight: ${() => style.font.weight.regular};
 `;
 
 const TextSmall = styled.p`
   font-family: ${() => style.font.light};
   font-size: ${() => style.font.size.small};
-  font-weight: ${() => style.font.weight.light};
+`;
+
+const TextBold = styled.p`
+  font-family: ${() => style.font.bold};
+  font-size: ${() => style.font.size.regular};
+`;
+
+const TextItalic = styled.p`
+  font-family: ${() => style.font.italic};
+  font-size: ${() => style.font.size.regular};
 `;
 
 const TitleSmall = styled.h5`
   font-family: ${() => style.font.medium};
   font-size: ${() => style.font.size.small};
   margin-bottom: ${() => style.font.size.small};
-  font-weight: ${() => style.font.weight.medium};
 `;
 
 const TitleRegular = styled.h4`
   font-family: ${() => style.font.bold};
   font-size: ${() => style.font.size.regular};
   margin-bottom: ${() => style.font.size.regular};
-  font-weight: ${() => style.font.weight.semiBold};
 `;
 
 const TitleMedium = styled.h3`
   font-family: ${() => style.font.bold};
   font-size: ${() => style.font.size.medium};
   margin-bottom: ${() => style.font.size.medium};
-  font-weight: ${() => style.font.weight.bold};
 `;
 
 const TitleLarge = styled.h2`
   font-family: ${() => style.font.heavy};
   font-size: ${() => style.font.size.large};
   margin-bottom: ${() => style.font.size.large};
-  font-weight: ${() => style.font.weight.extraBold};
 `;
 
 const TitleBig = styled.h1`
   font-family: ${() => style.font.extraBold};
   font-size: ${() => style.font.size.big};
   margin-bottom: ${() => style.font.size.medium};
-  font-weight: ${() => style.font.weight.black};
   text-shadow: ${() => style.shadow};
 `;
