149f82fc1288c516567d2aabe066323806cbcec6
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-01-31 17:44:49 +0100

    feta: update 404 page

--- src/routes/not-found/index.tsx
+++ src/routes/not-found/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import { SEO, Typography } from '../../components';
+import { Row, SEO, Typography } from '../../components';
 
 export const NotFound: React.FC = () => {
   return (
     <div>
       <SEO title='404: Page Not Found' description='...' keywords='...' />
-      <Typography type='title' variant='big'>
-        404: Page Not Found
-      </Typography>
+
+      <Component>
+        <Row>
+          <Typography type='title' variant='big'>
+            404
+          </Typography>
+          <Typography type='title' variant='medium'>
+            Page not found
+          </Typography>
+          <Link to='/'>Back to home</Link>
+        </Row>
+      </Component>
     </div>
   );
 };
+
+const Component = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
